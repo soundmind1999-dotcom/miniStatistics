16d1d30a943273d77b41d0af16be0ef417e1ec2b
soundmind1999-dotcom <soundmind1999@gmail.com>
2026-03-06 19:50:53 +0100

    Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -1,50 +1,36 @@
-public class Statistics{
+public class Statistics {
 
-    public static double mean (double [] numbers);
-
-        double sum = 0.0;
-        for (int count = 0; count < numbers.length; count++){
-        
-        sum += numbers[count];
-}
-        return sum;
-
-public static double mean (double [] numbers);
+    public static double mean(double[] numbers) {
 
         double sum = 0.0;
-        for (int count = 0; count < numbers.length; count++){
-        
-        sum += numbers[count];
-}
+
+        for (int count = 0; count < numbers.length; count++) {
+            sum += numbers[count];
+ }
+
         return sum / numbers.length;
+    }
 
 
-    public static double mean (double [] numbers);
-
-        double sum = 0.0;
-        for (int count = 0; count < numbers.length; count++){
-        
-        sum += numbers[count];
-}
-        return sum / numbers.length;
-
-
-
-    public static double variance (double [] numbers);
+    public static double variance(double[] numbers) {
 
         double mean = mean(numbers);
-        double sum = 0,0;
-        for (int count = 0; count < numbers.length; count++){
+        double sum = 0.0;
 
-        double difference = numbers[count] - mean;
-        sum += difference * difference;
+        for (int count = 0; count < numbers.length; count++) {
 
-}
+            double difference = numbers[count] - mean;
+            sum += difference * difference;
+
+      }
+
         return sum / numbers.length;
-
-    public static double standardDeviation(double [] numbers);
-
-        return Math.sqrt(variance(number));
+ }
 
 
+    public static double standardDeviation(double[] numbers) {
+
+        return Math.sqrt(variance(numbers));
+    
+}
 }
